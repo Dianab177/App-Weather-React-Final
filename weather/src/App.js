f9e4a3ed08c7c1693016c7dfc7b8dfbafcbaa161
DianaB177 <anaidb77@gmail.com>
2022-08-03 23:22:42 +0200

    Clean app- create build for deploy

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div class="container">
+         <small class="weather-link">
+         <a href="https://github.com/Dianab177/App-Weather-React-Final" target="_blank" title="link to github"  rel="noopener noreferrer">Open-source code</a>, by <a href="https://www.linkedin.com/in/dianaborrajo/">Diana Borrajo</a> 
+         </small>
+         </div>
+    </div>
+  );
+}
+
+export default App;
