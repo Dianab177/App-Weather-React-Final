0eba5a07d6b4f44a72e428208477f0e15e8f4a72
DianaB177 <anaidb77@gmail.com>
2022-08-04 14:05:11 +0200

    included css

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Weather from './Componetnts/Weather';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <div class="container">
         <Weather/>
          <small class="weather-link">
