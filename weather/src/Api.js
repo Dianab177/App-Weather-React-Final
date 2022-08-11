b9fea4603c4346575915320e21715e7756fc87f6
DianaB177 <anaidb77@gmail.com>
2022-08-12 00:21:51 +0200

    api build

---
+++ weather/src/Api.js
@@ -0,0 +1,2 @@
+let urlForescat = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apikey}`;
+let apiKey = "094780c710fa4efd669f0df8c3991927";
--- weather/src/WeatherForecast.js
+++ weather/src/WeatherForecast.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import WeatherIcon from './WeatherIcon'
 import "./WeatherForecast.css";
+import { axios } from 'axios';
 const WeatherForecast = () => {
+function handleResponse(response) {
+  console.log(response.data);
+}
+
+let longitude = 40.7;
+let latutude = 74;
+let urlForescat = `https://api.openweathermap.org/data/3.0/onecall?lat=${laitude}&lon=${longitude}&exclude={part}&appid=${apikey}`;
+let apiKey = "094780c710fa4efd669f0df8c3991927";
+axios.length(urlForescat).then(handleResponse);
   return (
     <div className='WeatherForecast'>
         <div className='row'>
         <div className='col'>
             <div className='WeatherForecastDay'>Thu</div>
             <div><WeatherIcon code="01d" size={36}/></div>
-            <div className='WeatherForecastTemperature'>
+              <div className='WeatherForecastTemperature'>
                 <span className='WeatherForecastTemperature-max'>19º</span>
                 <span className='WeatherForecastTemperature-min'>10º</span>
-            </div>
+              </div>
         </div>
         </div>
         
